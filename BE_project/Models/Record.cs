e7d039d3e7c50a23aaf4946bacd7cfaea9f3f2f0
Sonik Kulichenko <sofiy.kulichenko@gmail.com>
2025-10-17 16:04:05 +0300

    Template cleanup + models structure
    Видалення лишніх автоматично створених файлів з темплейту. Визначення структури проекту(розбиття на папки). Додання моделей Користувачів, Категорій та Записів

---
+++ BE_project/Models/Record.cs
@@ -0,0 +1,11 @@
+﻿namespace BE_project.Models
+{
+    public class Record
+    {
+        public int Id { get; set; }
+        public int UserId { get; set; }
+        public int CategoryId { get; set; }
+        public DateTime DateTime { get; set; }
+        public decimal Amount { get; set; }
+    }
+}
